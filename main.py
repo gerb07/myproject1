fa1113d2dbeac56e072d553e02f694559ae2fbc1
gerb07 <markgerber13@icloud.com>
2024-06-30 12:41:54 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+print("hello world")
+
+exemple = 'гениальный'
+print(exemple[0])
+print(exemple[-1])
+print(exemple[5 : ])
+print(exemple[ ::-1])
+print(exemple[0:10:2])
+
+
+number_of_completed_dz = 12
+number_of_hours_spent = 1.5
+name_of_the_cource = 'Python'
+time_for_one_task = 10.5
+
+print(number_of_completed_dz - number_of_hours_spent)
+print(name_of_the_cource + ',' + str(number_of_completed_dz) + ',' + str(number_of_hours_spent) + ',' + str(time_for_one_task))
+
+
